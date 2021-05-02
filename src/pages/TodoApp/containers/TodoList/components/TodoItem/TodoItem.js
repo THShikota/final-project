56b7a9a9e518d7aa08943bba992acbee7cd3b975
Tiago Shikota <th.shikota@gmail.com>
2021-05-02 15:07:18 -0300

    Projeto React Finalizado

---
+++ src/pages/TodoApp/containers/TodoList/components/TodoItem/TodoItem.js
@@ -0,0 +1,44 @@
+import React, { useState, useCallback, useEffect } from 'react'
+import { ReactComponent as UpdateIcon } from '../../../../../../assets/icons/update-icon.svg'
+import { ReactComponent as DeleteIcon } from '../../../../../../assets/icons/delete-icon.svg'
+import styles from './TodoItem.module.css'
+
+function TodoItem({ id, title, completed, onModalOpen, onStatusUpdate, onDelete }) {
+    const [isChecked, setIsChecked] = useState(completed)
+    const handleChange = useCallback(
+        (evt) => {
+            setIsChecked(evt.target.checked)
+        },
+        [setIsChecked],
+    )
+    useEffect(() => {
+        onStatusUpdate(id, isChecked)
+    } , [onStatusUpdate, id, isChecked])
+
+    const handleModalOpen = useCallback(
+        () => {
+            onModalOpen(id)
+        },
+        [onModalOpen, id],
+    )
+
+    const handleDelete = useCallback(
+        () => {
+            onDelete(id)
+        },
+        [onDelete, id],
+    )
+    return (
+        <li className={styles.item}>
+            <span className={completed ? styles.completed : null}>{title}</span>
+            <div className={styles.controlButtons}>
+                <button onClick={handleModalOpen}><UpdateIcon /></button>
+                <input type="checkbox" checked={isChecked} onChange={handleChange} />
+                <button onClick={handleDelete}><DeleteIcon /></button>
+            </div>
+
+        </li>
+    )
+}
+
+export default TodoItem
